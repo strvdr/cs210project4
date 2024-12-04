78bd9e562cab9f7d7780f911eb9f58ab5415e422
Strydr <strydr8@gmail.com>
2024-12-04 08:16:34 -0700

    working ~not really~ server.c

--- server.c
+++ server.c
@@ -1,57 +1,46 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <string.h>
 #include <signal.h>
 
 struct message {
-	char source[50];
-	char target[50]; 
-	char msg[200]; // message body
+    char source[50];
+    char target[50]; 
+    char msg[200]; // message body
 };
 
 void terminate(int sig) {
-	printf("Exiting....\n");
-	fflush(stdout);
-	exit(0);
+    printf("Exiting...\n");
+    fflush(stdout);
+    exit(0);
 }
 
 int main() {
-	int server;
-	int target;
-	int dummyfd;
-	struct message req;
-	signal(SIGPIPE,SIG_IGN);
-	signal(SIGINT,terminate);
-	server = open("serverFIFO",O_RDONLY);
-	dummyfd = open("serverFIFO",O_WRONLY);
+  int target;
+  int server;
+  int dummyfd;
 
-	while (1) {
-		// TODO:
-		// read requests from serverFIFO
+  struct message req;
 
+  signal(SIGPIPE, SIG_IGN);  // Ignore broken pipe signals
 
+  // Open the server FIFO for reading and a dummy write to keep it open
+  server = open("serverFIFO", O_RDONLY);
+  dummyfd = open("serverFIFO", O_WRONLY);
 
+  while (1) {
+      // Read requests from serverFIFO
+      if(read(server, &req, sizeof(struct message)) != sizeof(struct message)) continue;
+      printf("Received a request from %s to send the message %s to %s.\n", req.source, req.msg, req.target);
 
-
-
-		printf("Received a request from %s to send the message %s to %s.\n",req.source,req.msg,req.target);
-
-		// TODO:
-		// open target FIFO and write the whole message struct to the target FIFO
-		// close target FIFO after writing the message
-
-
-
-
-
-
-
-	}
-	close(server);
-	close(dummyfd);
-	return 0;
+      // Open target FIFO and write the whole message struct to the target FIFO
+      target = open(req.target, O_WRONLY);
+       
+      write(target, &req, sizeof(struct message));
+      close(target); // Close target FIFO after writing
+  }
+  close(server);
+  return 0;
 }
 
